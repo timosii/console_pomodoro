c7d9965a644ae4f46339f8df9691a5fee691d845
timosii <holdcarter@holdcarter.ru>
2023-07-02 15:16:22 +0300

    add logging

--- pomodoro/logic.py
+++ pomodoro/logic.py
@@ -1,15 +1,25 @@
 import time
 
+LOGS_PATH = "logging/logs"
 
-def time_control(t, indicator_length, display='#'):
+
+def time_control(t, indicator_length=60, display='#'):
     '''
     На вход поступает необходимое время в минутах.
     Возвращает None, заканчивает работу, спустя это время
     '''
     time_sec = 60 * t
-    cycle = time_sec // indicator_length
+    cycle = time_sec / indicator_length
+    
     for _ in range(indicator_length):
-        print(display, flush=True, end=' ')
+        print(display, flush=True, end='')
         time.sleep(cycle)
 
-        
+    print()
+    TEXT = f'Cycle of {t} minutes complete at {time.asctime()}'
+    write_logs(LOGS_PATH, TEXT)
+
+
+def write_logs(filename, text):
+    with open(filename, 'a') as file:
+        file.write(text)
--- pomodoro/parser_args.py
+++ pomodoro/parser_args.py
@@ -3,8 +3,8 @@ import argparse
 def take_args():
     parser = argparse.ArgumentParser(description="Use your console for time managment")
     parser.add_argument('time_in_minutes', type=int, help='duration of one cycle')
-    parser.add_argument('indicator_lenght', type=int, \
-                        help='the length of the strip of one cycle (in chars)')
+    parser.add_argument('-il', '--indicator_lenght', type=int, help='the length \
+    of the strip of one cycle (in chars)', default=60)
     parser.add_argument('-d', '--display', help='choose your indicator', default='#')
     args = parser.parse_args()
     time = args.time_in_minutes
