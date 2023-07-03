46f859cfc960ecec041e0e9daf984b7b8b1951b2
timosii <holdcarter@holdcarter.ru>
2023-07-03 14:10:52 +0300

    MVP version

--- pomodoro/logic.py
+++ pomodoro/logic.py
@@ -2,19 +2,16 @@ import time
 
 LOGS_PATH = "logging/logs"
 
-
 def time_control(t, indicator_length=60, display='#'):
     '''
     На вход поступает необходимое время в минутах.
     Возвращает None, заканчивает работу, спустя это время
     '''
-    time_sec = 60 * t
-    cycle = time_sec / indicator_length
+    cycle = 60 * t / indicator_length
     
     for _ in range(indicator_length):
         print(display, flush=True, end='')
         time.sleep(cycle)
-
     print()
     TEXT = f'Cycle of {t} minutes complete at {time.asctime()}'
     write_logs(LOGS_PATH, TEXT)
