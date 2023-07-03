126700af95716dfbde6c641a551e1757d1db0381
timosii <holdcarter@holdcarter.ru>
2023-07-03 17:32:07 +0300

    add color intervals

--- pomodoro/logic.py
+++ pomodoro/logic.py
@@ -10,19 +10,27 @@ def time_control(t, length_indicator=60, display=' '):
     Возвращает None, заканчивает работу, спустя это время
     '''
     cycle = 60 * t / length_indicator
-    point_1 = length_indicator // 3
+    point_1 = length_indicator / 5
     point_2 = point_1 * 2
+    point_3 = point_1 * 3
+    point_4 = point_1 * 4
     for i in range(length_indicator):
         if i <= point_1:
-            print(Back.GREEN + display, flush=True, end='')
+            print(Back.LIGHTGREEN_EX + display, flush=True, end='')
             time.sleep(cycle)
         elif point_1 < i <= point_2:
+            print(Back.GREEN + display, flush=True, end='')
+            time.sleep(cycle)
+        elif point_2 < i <= point_3:
             print(Back.YELLOW + display, flush=True, end='')
+            time.sleep(cycle)
+        elif point_3 < i <= point_4:
+            print(Back.MAGENTA + display, flush=True, end='')
             time.sleep(cycle)
         else:
             print(Back.RED + display, flush=True, end='')
             time.sleep(cycle)
-    print()
+    print(Back.RESET + '\nCycle finished. Take a rest!')
     playsound("finish_cycle.mp3")
     TEXT = f'Cycle of {t} minutes complete at {time.asctime()}\n'
     write_logs(LOGS_PATH, TEXT)
