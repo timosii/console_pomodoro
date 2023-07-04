0102ca6d5d824bbb162da352c9646a04daafece5
timosii <holdcarter@holdcarter.ru>
2023-07-04 18:37:33 +0300

    correct interruption

--- pomodoro/logic.py
+++ pomodoro/logic.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import time
 from colorama import Back
 from playsound import playsound
@@ -14,39 +15,44 @@ def time_control(t, length_indicator=60, display=' '):
     point_2 = point_1 * 2
     point_3 = point_1 * 3
     point_4 = point_1 * 4
-    cycles = 0
-    for i in range(length_indicator):
-        try:
+    start_time = dt.datetime.now()
+    try:
+        for i in range(length_indicator):
             if i <= point_1:
                 print(Back.LIGHTGREEN_EX + display, flush=True, end='')
                 time.sleep(cycle)
+                
             elif point_1 < i <= point_2:
                 print(Back.GREEN + display, flush=True, end='')
                 time.sleep(cycle)
+                
             elif point_2 < i <= point_3:
                 print(Back.YELLOW + display, flush=True, end='')
                 time.sleep(cycle)
+                
             elif point_3 < i <= point_4:
                 print(Back.MAGENTA + display, flush=True, end='')
                 time.sleep(cycle)
+                
             else:
                 print(Back.RED + display, flush=True, end='')
                 time.sleep(cycle)
-            cycles += 1
 
-        except KeyboardInterrupt:
-            stop_time = cycles // 60
-            print(Back.RESET + f'\nCycle interruption at {stop_time} minute')
-            write_logs(LOGS_PATH, f'Cycle of {stop_time} minutes complete at {time.asctime()}. Interruption\n')
-            return
 
+    except KeyboardInterrupt:
+        stop_time = dt.datetime.now()
+        time_diff = stop_time - start_time
+        time_diff_in_min = time_diff.seconds // 60
+        print(Back.RESET + f'\nCycle interruption at {time_diff_in_min} minute')
+        write_logs(LOGS_PATH, f'Cycle of {time_diff_in_min} minutes complete at {time.asctime()}. Interruption\n')
+        return
 
 
     print(Back.RESET + f'\nCycle of {t} minutes finished. Take a rest!')
     playsound("finish_cycle.mp3")
     TEXT = f'Cycle of {t} minutes complete at {time.asctime()}\n'
     write_logs(LOGS_PATH, TEXT)
-
+        
 
 def write_logs(filename, text):
     with open(filename, 'a') as file:
